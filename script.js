61a0dfc2d9ff64377f557a07ffe156045e9fb5e6
tore aldemir <aldemirtore@gmai.com>
2023-04-09 19:43:22 +0300

    second commit some styles and start of the js

---
+++ script.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-unused-vars */
+const library = document.querySelector('.library');
+const addButton = document.querySelector('.addButton');
+const overlay = document.querySelector('.form-div');
